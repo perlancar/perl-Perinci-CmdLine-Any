1599fbe63b2b73f2f84f5ad7ec1475cd52022b4f
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-07-16 22:52:11 +0700

    POD

--- lib/Perinci/CmdLine/Any.pm
+++ lib/Perinci/CmdLine/Any.pm
@@ -31,13 +31,13 @@ if ($@) {
 
 =head1 DESCRIPTION
 
-Not unlike L<Any::Moose>, this module lets you use L<Perinci::CmdLine> if it's
-available, or L<Perinci::CmdLine::Lite> as the fallback. The goal is to reduce
-dependencies (Perinci::CmdLine::Any only depends on the lightweight
-Perinci::CmdLine::Lite) but use the richer Perinci::CmdLine if it's available.
+This module lets you use L<Perinci::CmdLine> if it's available, or
+L<Perinci::CmdLine::Lite> as the fallback. The goal is to reduce dependencies
+(Perinci::CmdLine::Any only depends on the lightweight Perinci::CmdLine::Lite)
+but use the richer Perinci::CmdLine if it's available.
 
-Note that Perinci::CmdLine::Lite is not 100% API-compatible with
-Perinci::CmdLine.
+Note that Perinci::CmdLine::Lite provides only a subset of the
+functionalities/features of Perinci::CmdLine.
 
 
 =head1 SEE ALSO
